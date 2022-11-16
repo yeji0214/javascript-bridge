d0be6a13b6bab2c763e0a75599e360da3f7d0fea
peacemaker474 <81507322+peacemaker474@users.noreply.github.com>
2022-11-16 16:08:42 +0900

    Test: 이동 방향에 대한 테스트 추가
    추후 배열 형태로 테스트 진행

--- __tests__/InputTest.js
+++ __tests__/InputTest.js
@@ -1,4 +1,7 @@
-const { validateBridgeSize } = require("../src/errorHandling");
+const {
+  validateBridgeSize,
+  validateInputBridgeMoving,
+} = require("../src/errorHandling");
 
 describe("다리 길이 입력에 대한 유효성 검사", () => {
   test("다리의 길이가 3이상 20이하인가?", () => {
@@ -10,6 +13,14 @@ describe("다리 길이 입력에 대한 유효성 검사", () => {
   test("다리 길이 입력의 숫자 이외의 것이 있는가?", () => {
     expect(() => {
       validateBridgeSize("10.");
+    }).toThrow("[ERROR]");
+  });
+});
+
+describe("이동 방향 입력에 대한 유효성 검사", () => {
+  test("올바른 이동 방향을 입력했는가?", () => {
+    expect(() => {
+      validateInputBridgeMoving("r");
     }).toThrow("[ERROR]");
   });
 });
