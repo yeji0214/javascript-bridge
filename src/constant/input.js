b3611cd17d066d81c58d0001355e56398be3e134
peacemaker474 <81507322+peacemaker474@users.noreply.github.com>
2022-11-16 15:33:52 +0900

    Feat: 예외 및 다리 길이에 대한 입력 메세지 추가

---
+++ src/constant/input.js
@@ -0,0 +1,14 @@
+const MESSAGE = {
+  game_start: `다리 건너기 게임을 시작합니다.\n`,
+  input_bridge: `다리의 길이를 입력해주세요.\n`,
+};
+
+const RANGE = {
+  min: 3,
+  max: 20,
+};
+
+module.exports = {
+  MESSAGE,
+  RANGE,
+};
