4ccea8e066bf7b6de028d6cd2ef5ccea1bbfa962
yeji0214 <1225yj50@naver.com>
2024-12-13 13:19:25 +0900

    Initial setting

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
-const InputView = require('./InputView');
-
 class App {
   play() {
-    InputView.readGameStart();
-    InputView.readBridgeSize();
+
   }
 }
 
 const app = new App();
 
 app.play();
-
-module.exports = App;
